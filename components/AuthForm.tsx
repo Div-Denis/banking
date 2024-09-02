1f70a8860587c29e916ed7932170eaf9f54dda65
denisdiv <djw19910815@gmail.com>
2024-09-02 21:12:09 +0800

    Implement plaid link and Dwolla

--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -24,6 +24,7 @@ import { authFormSchema } from '@/lib/utils'
 import { Loader2 } from 'lucide-react'
 import { useRouter } from 'next/navigation'
 import { getLoggedInUser, signIn, signUp } from '@/lib/actions/user.actions'
+import PlaidLink from './PlaidLink'
 
 
 const AuthForm = ({ type } : { type: string }) => {
@@ -48,9 +49,21 @@ const AuthForm = ({ type } : { type: string }) => {
     setIsLoading(true)
     try {
       // Sign up with Appwrite & create plain plain token
-
+      
       if(type === 'sign-up'){
-        const newUser = await signUp(data)
+        const userData = {
+          firstName: data.firstName!,
+          lastName: data.lastName!,
+          address1: data.address1!,
+          city: data.city!,
+          state: data.state!,
+          postalCode: data.postalCode!,
+          dateOfBirth: data.dateOfBirth!,
+          ssn: data.ssn!,
+          email: data.email,
+          password: data.password,
+        }
+        const newUser = await signUp(userData)
 
         setUser(newUser)
       }
@@ -104,8 +117,9 @@ const AuthForm = ({ type } : { type: string }) => {
         {user ? (
             <div className='flex flex-col gap-4'>
                 {/* plaidLink */}
+                <PlaidLink user={user} variant='primary' />
             </div>
-        ): (
+        ): ( 
             <>
                <Form {...form}>
                     <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
@@ -263,7 +277,7 @@ const AuthForm = ({ type } : { type: string }) => {
                     </Link>
                 </footer>
             </>
-        )}
+        )} 
     </section>
   )
 }
--- types/index.d.ts
+++ types/index.d.ts
@@ -9,14 +9,14 @@ declare type SearchParamProps = {
 
 // 注册用户信息
 declare type SignUpParams = {
-  firstName?: string;
-  lastName?: string;
-  address1?: string;
-  city?: string;
-  state?: string;
-  postalCode?: string;
-  dateOfBirth?: string;
-  ssn?: string;
+  firstName: string;
+  lastName: string;
+  address1: string;
+  city: string;
+  state: string;
+  postalCode: string;
+  dateOfBirth: string;
+  ssn: string;
   email: string;
   password: string;
 };
